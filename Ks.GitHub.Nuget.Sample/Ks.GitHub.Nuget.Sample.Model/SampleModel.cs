b05d044de71137a383c747ddc34ea31e9bf17d21
no-okada <no-okada@kentem.co.jp>
2023-01-20 15:10:00 +0900

    基盤作成

---
+++ Ks.GitHub.Nuget.Sample/Ks.GitHub.Nuget.Sample.Model/SampleModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ks.GitHub.Nuget.Sample.Model
+{
+    public class SampleModel
+    {
+        /// <summary>ID</summary>
+        public Guid Id { get; set; }
+        /// <summary>名前</summary>
+        public string Name { get; set; }
+        /// <summary>説明</summary>
+        public string Description { get; set; }
+
+    }
+}
